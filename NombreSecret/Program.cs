8757a82b1a562501ce987e08799acfee6b349636
karimdevwm <ka.trabelsi9@gmail.com>
2021-05-11 19:21:16 +0200

    Ajoutez des fichiers projet.

---
+++ NombreSecret/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NombreSecret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int nbrSecret = 56;
+            bool gagne = false;
+            int saisie = 0;
+            int essais = 0;
+            var generator = new Random();
+            Console.WriteLine("règle...");
+
+            while(gagne!=true)
+            {
+                saisie = Convert.ToInt32(Console.ReadLine());
+                essais += 1;
+                if(saisie==nbrSecret)
+                {
+                    Console.WriteLine("gagne !!!");
+                    Console.WriteLine("vous avez trouvé en ", essais, " essais");
+                    gagne = true;
+                }
+                else
+                {
+                    if(saisie<nbrSecret)
+                    {
+                        Console.WriteLine("le nombre a trouvé est supérieur");
+                    }
+                    else
+                    {
+                        if(saisie>nbrSecret)
+                        {
+                            Console.WriteLine("le nombre a trouvé est inférieur");
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("à la prochaine");
+        }
+    }
+}
